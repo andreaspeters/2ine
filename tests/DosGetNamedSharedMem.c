75c87013e15c3033dd7adcc3c0c563c8c2a00d80
Andreas Peters <ap@aventer.biz>
2021-12-31 00:51:18 +0100

    add DosGetNamedSharedMem test file

--- tests/DosGetNamedSharedMem.c
+++ tests/DosGetNamedSharedMem.c
@@ -1,21 +1,34 @@
 #define INCL_DOSMEMMGR
-#include <bsememf.h>
+#define INCL_DOSFILEMGR   /* File Manager values */
+#define INCL_DOSERRORS    /* DOS error values    */
 #include <os2.h>
+#include <stdio.h>
+#include <string.h>
+
 
 int main(VOID) {
   PVOID BaseAddress;
   UCHAR Name[40];
   ULONG AttributeFlags;
   APIRET rc;
-  
+   
   strcpy(Name,"\\SHAREMEM\\BLOCK.DAT");   
-  
-  AttributeFlags = PAG_WRITE | PAG_READ | PAG_COMMIT;
-  
+  AttributeFlags = 0x00000001;
+
+  rc = DosAllocSharedMem(&BaseAddress, Name, 8192, AttributeFlags);
+  if (rc != 0)
+  {
+    printf("DosAllocShrSeq error: return code = %u\n", rc);
+    /* We have an error we must take care of. */
+  }
+
   rc = DosGetNamedSharedMem( &BaseAddress, Name, AttributeFlags);
   
   if (rc != 0)
   {
-     /* We have an error we must take care of. */
+    printf("DosGetNameSharedMem error: return code = %u\n", rc);
+    /* We have an error we must take care of. */
   }
+  printf("OK rc = %u\n", rc);
+  return rc;
 }
